fe1f1446b4271ca12c2f9025a55d213b98276ef5
urian <urian1213viera@gmail.com>
2022-12-25 19:08:28 -0500

    agregando loader al proyecto

---
+++ app/static/assets/js/loader.js
@@ -0,0 +1,5 @@
+/*Apenas termine de cargar el documento entra aqui*/    
+window.addEventListener('load', (event) => {
+    let miBody = document.body;
+       miBody.classList.remove('loader');
+});
